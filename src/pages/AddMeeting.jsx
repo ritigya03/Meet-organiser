aac0a5dc1eb8f6eb676cc6819e98971ac5b91695
Ritigya Gupta <ritigya00003@gmail.com>
2024-07-10 15:30:44 +0530

    second commit

--- src/pages/AddMeeting.jsx
+++ src/pages/AddMeeting.jsx
@@ -13,6 +13,7 @@ function AddMeeting() {
     let [loadingStatus, setLoadingStatus] = useState(false);
     let navigate = useNavigate();
     const { theme, toggleTheme } = useTheme();
+
     function addMeetingHandler() {
         setLoadingStatus(true);
         let newMeetingData = {
@@ -21,6 +22,7 @@ function AddMeeting() {
             desc: descInput.current.value,
             type: typeInput.current.value
         };
+
         fetch('https://meet-app-dbc6c-default-rtdb.firebaseio.com/meetings.json', {
             method: 'post',
             body: JSON.stringify(newMeetingData)
@@ -31,33 +33,33 @@ function AddMeeting() {
     }
 
     return (
-        <div className="min-h-screen flex flex-col">
-    
-
-            <section className="max-w-screen-l mx-auto flex-grow text-left px-4 lg:px-8">
+        <div className="min-h-screen  flex flex-col">
+            <section className="max-w-screen-sm mx-auto flex-grow text-left px-4 lg:px-8">
                 <div className="text-center">
-                    <div className="w-[400px] text-left mx-auto border-black px-6 py-8 rounded-xl">
+                    <div className="w-full max-w-[400px] mx-auto border-black px-6 py-8 rounded-xl">
                         <div className="flex justify-center">
-                            <Link to={'/'}><img className={theme === 'light' ? 'w-[80px] h-[80px] mb-5 ' : 'w-[80px] h-[80px] mb-5 bg-pink-100'} src={HappyPanda} alt="" /></Link>
+                            <Link to={'/'}>
+                                <img className={theme === 'light' ? 'w-[80px] h-[80px] mb-5' : 'w-[80px] h-[80px] mb-5 bg-pink-100'} src={HappyPanda} alt="Happy Panda" />
+                            </Link>
                         </div>
                         <h1 className="text-4xl font-black text-center">Add new meeting</h1>
 
                         <div className="w-full mt-6">
-                            <input ref={titleInput} type="text" placeholder="Meeting Title" className="flex w-full h-10 px-3 py-6 text-sm bg-white border rounded-lg border-neutral-300 ring-offset-background placeholder:text-black focus:border-neutral-300 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-neutral-400 disabled:cursor-not-allowed disabled:opacity-50" />
+                            <input ref={titleInput} type="text" placeholder="Meeting Title" className="w-full h-10 px-3 py-2 text-sm bg-white border rounded-lg border-neutral-300 ring-offset-background placeholder-text-black focus:border-neutral-300 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-neutral-400 disabled:cursor-not-allowed disabled:opacity-50" />
                         </div>
                         <div className="w-full mt-6">
-                            <input ref={dateInput} type="datetime-local" placeholder="Meeting Date" className="flex w-full h-10 px-3 py-6 text-sm bg-white border rounded-lg border-neutral-300 ring-offset-background placeholder:text-black focus:border-neutral-300 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-neutral-400 disabled:cursor-not-allowed disabled:opacity-50" />
+                            <input ref={dateInput} type="datetime-local" placeholder="Meeting Date" className="w-full h-10 px-3 py-2 text-sm bg-white border rounded-lg border-neutral-300 ring-offset-background placeholder-text-black focus:border-neutral-300 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-neutral-400 disabled:cursor-not-allowed disabled:opacity-50" />
                         </div>
                         <div className="w-full mt-6">
-                            <input ref={typeInput} type="text" placeholder="Topic(i.e Dev, Operations, Marketing...)" className="flex w-full h-10 px-3 py-6 text-sm bg-white border rounded-lg border-neutral-300 ring-offset-background placeholder:text-black focus:border-neutral-300 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-neutral-400 disabled:cursor-not-allowed disabled:opacity-50" />
+                            <input ref={typeInput} type="text" placeholder="Topic (e.g., Dev, Operations, Marketing...)" className="w-full h-10 px-3 py-2 text-sm bg-white border rounded-lg border-neutral-300 ring-offset-background placeholder-text-black focus:border-neutral-300 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-neutral-400 disabled:cursor-not-allowed disabled:opacity-50" />
                         </div>
                         <div className="w-full mt-6">
-                            <input ref={descInput} type="text" placeholder="Desc" className="flex w-full h-10 px-3 py-6 text-sm bg-white border rounded-lg border-neutral-300 ring-offset-background placeholder:text-black focus:border-neutral-300 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-neutral-400 disabled:cursor-not-allowed disabled:opacity-50" />
+                            <input ref={descInput} type="text" placeholder="Description" className="w-full h-10 px-3 py-2 text-sm bg-white border rounded-lg border-neutral-300 ring-offset-background placeholder-text-black focus:border-neutral-300 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-neutral-400 disabled:cursor-not-allowed disabled:opacity-50" />
                         </div>
                         <div className="flex justify-center">
-                            <button onClick={addMeetingHandler} className={theme === 'light' ? "bg-black px-12 py-4 text-white mt-5 rounded-lg flex gap-4 items-center" : "bg-pink-200 px-12 py-4 text-black weight-bolder mt-5 rounded-lg flex gap-4 items-center"}>
+                            <button onClick={addMeetingHandler} className={theme === 'light' ? "bg-black px-6 py-3 mt-5 rounded-lg flex gap-2 items-center text-white" : "bg-pink-200 px-6 py-3 mt-5 rounded-lg flex gap-2 items-center text-black"}>
                                 <span>Create Meeting</span>
-                                <span className={loadingStatus ? "loader" : ""}></span>
+                                {loadingStatus && <span className="loader"></span>}
                             </button>
                         </div>
                     </div>
